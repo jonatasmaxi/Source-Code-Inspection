f59b87ecab6475556dfec173b9e075497591b9b8
ta-ma <ta-ma@DESKTOP-70FFDS2>
2016-03-27 09:51:01 -0300

    Correção dos Loops Infintos presentes na classe. Correção do construtor. Inclusão do método contaNotas que antes era executado pelo construtor.

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -8,41 +8,46 @@ import java.util.Iterator;
  */
 public class Troco {
 
-    protected PapelMoeda[] papeisMoeda;
+    protected static PapelMoeda[] papeisMoeda;
+    private int valor;
 
     public Troco(int valor) {
-        papeisMoeda = new PapelMoeda[6];
-        int count = 0;
-        while (valor % 100 != 0) {
-            count++;
-        }
-        papeisMoeda[5] = new PapelMoeda(100, count);
-        count = 0;
-        while (valor % 50 != 0) {
-            count++;
-        }
-        papeisMoeda[4] = new PapelMoeda(50, count);
-        count = 0;
-        while (valor % 20 != 0) {
-            count++;
-        }
-        papeisMoeda[3] = new PapelMoeda(20, count);
-        count = 0;
-        while (valor % 10 != 0) {
-            count++;
-        }
-        papeisMoeda[2] = new PapelMoeda(10, count);
-        count = 0;
-        while (valor % 5 != 0) {
-            count++;
-        }
-        papeisMoeda[1] = new PapelMoeda(5, count);
-        count = 0;
-        while (valor % 2 != 0) {
-            count++;
-        }
-        papeisMoeda[1] = new PapelMoeda(2, count);
+        papeisMoeda = new PapelMoeda[7];
+        this.valor = valor;
+        contaNotas();
+    }
 
+    private void contaNotas() {
+        while (valor > 0) {
+            if (valor / 100 != 0) {
+                papeisMoeda[6] = new PapelMoeda(100, valor / 100);
+                valor = valor % 100;
+            }
+            if (valor / 50 != 0) {
+                papeisMoeda[5] = new PapelMoeda(50, valor / 50);
+                valor = valor % 50;
+            }
+            if (valor / 20 != 0) {
+                papeisMoeda[4] = new PapelMoeda(20, valor / 20);
+                valor = valor % 20;
+            }
+            if (valor / 10 != 0) {
+                papeisMoeda[3] = new PapelMoeda(10, valor / 10);
+                valor = valor % 10;
+            }
+            if (valor / 5 != 0) {
+                papeisMoeda[2] = new PapelMoeda(10, valor / 5);
+                valor = valor % 5;
+            }
+            if (valor / 2 != 0) {
+                papeisMoeda[1] = new PapelMoeda(10, valor / 2);
+                valor = valor % 2;
+            }
+            if (valor / 1 != 0) {
+                papeisMoeda[0] = new PapelMoeda(10, valor / 1);
+                valor = valor % 1;
+            }
+        }
     }
 
     public PapelMoeda[] getPapeisMoeda() {
